b9c2cd4bbec071b239fcfc5391bd0d64e5b42754
Ayush Agrawal <111371722+ayush360@users.noreply.github.com>
2023-08-23 00:43:56 +0530

    900

---
+++ CodeForces/CodeForces_900/34B_Sale.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    int n , k ;
+    cin>>n >>k ;
+    int a[n] ;
+    for(int i=0; i<n; i++) cin>>a[i] ;
+    sort(a, a+n) ;
+    int sum=0 ;
+    for(int i=0; i<k; i++) {
+        if(a[i] >=0)break ;
+        sum+= a[i]  ;
+    }
+    cout<<-sum ;
+}
